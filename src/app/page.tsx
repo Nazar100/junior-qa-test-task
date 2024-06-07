f3b82b52997627fcbe1a0ad35cee2f598995368d
Nazar Leshchyshyn <65277641+Nazar100@users.noreply.github.com>
2024-06-07 14:20:47 +0300

    added form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,60 @@
 "use client"
-import styles from "./page.module.css";
-import { useState } from "react";
+import axios from "axios";
+import styles from "./page.module.scss";
+import { useEffect, useState } from "react";
+const TICKETS_NUMBER = 121;
 
 export default function Home() {
-  const [input, setInput] = useState(0);
+  const [ticketsNumber, setTicketsNumber] = useState(1);
+  const [ticketsLeft, setTicketsLeft] = useState(TICKETS_NUMBER);
+  const [ticketsPrice, setTicketsPrice] = useState(10)
+
   const handleInputChange = (e: any) => {
-    setInput(e.target.value)
+    const number = Number(e.target.value);
+
+    if (number >= 0 && number <= TICKETS_NUMBER + 1) {
+      setTicketsNumber(e.target.value);
+    }
+
   }
+
+  useEffect(() => {
+    if (ticketsNumber >= 0 && ticketsNumber <= 10) {
+      setTicketsPrice(10)
+    } else if (ticketsNumber >= 11 && ticketsNumber <= 20) {
+      setTicketsPrice(9)
+    }
+    else if (ticketsNumber >= 21 && ticketsNumber <= 35) {
+      setTicketsPrice(8)
+
+    } else {
+      setTicketsPrice(7)
+    }
+  }, [ticketsNumber]);
+  const handleButtonClick = () => {
+    console.log(typeof ticketsNumber, ticketsNumber);
+    try {
+      axios.post('/test-api', { ticketsNumber: Number(ticketsNumber) + 1 })
+    } catch (e) { }
+  }
+
+
   return (
     <main className={styles.main}>
-      <input value={input} onChange={handleInputChange} />
+      <div>
+        <h1>Tickets</h1>
+        <h2>Resarve tickets reserve for 5 minutes</h2>
+        <p>Tickets left: {TICKETS_NUMBER}</p>
 
-      <h1>{input}</h1>
+        <input value={ticketsNumber} onChange={handleInputChange} placeholder="Choose tickets number" type="number" />
+
+        <p>Price: {ticketsPrice}</p>
+        <p>Total Price:{ticketsNumber * ticketsPrice}</p>
+        <button onClick={handleButtonClick} className={styles.button}>
+          RESERVE
+        </button>
+      </div>
+      <p className={styles.invisible}>TODO: Add Footer</p>
     </main>
   );
 }
